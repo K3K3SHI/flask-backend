81288401b6fc302a5167a45c8178f3e8496ef394
Keshav Ramakrishnan <keshavr.email@gmail.com>
2025-11-10 05:27:14 +0530

    Initial commit

--- app.py
+++ app.py
@@ -189,4 +189,5 @@ def update_order_status_route():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
